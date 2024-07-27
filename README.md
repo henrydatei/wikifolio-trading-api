a5a3f23310428dda6dbaa442b8cfb6cc841e3127
henrydatei <31316025+henrydatei@users.noreply.github.com>
2024-07-27 17:10:39 +0200

    add limit and quote orders, fix #2

--- README.md
+++ README.md
@@ -27,6 +27,10 @@ from wikifolioTradingAPI import WikifolioTradingAPI
 wf_api = WikifolioTradingAPI("my_client_api_key", "my_user_api_key")
 print(wf_api.list_wikifolios())
 print(wf_api.get_wikifolio('wf0spc2022'))
-# print(wf_api.list_wikifolio_underlyings('wf0spc2022')) # Was not working when I tested it
+# print(wf_api.list_wikifolio_underlyings('wf0spc2022')) # It says it found 2677 underlyings??? This wikifolio has 8 positions. Maybe itz lists all possible underlyings?
 print(wf_api.list_wikifolio_orders('wf0spc2022'))
+orderid = wf_api.place_limit_order('wf0spc2022', 'US0378331005', 1, 150, datetime.date(2024,7,28) ,'buy')
+print(wf_api.get_limit_order(orderid))
+wf_api.delete_limit_order(orderid)
+print(wf_api.place_quote_order('wf0spc2022', 'US0378331005', 1, 'buy'))
 ```
--- wikifolioTradingAPI/wikifolio.py
+++ wikifolioTradingAPI/wikifolio.py
@@ -4,6 +4,7 @@ from dacite import from_dict
 from typing import List, Optional, Tuple
 import logging
 import coloredlogs
+import datetime
 
 from classes.WikifolioListItem import WikifolioListItem
 from classes.Wikifolio import Wikifolio
@@ -11,7 +12,7 @@ from classes.Underlying import Underlying
 from classes.OrderStatusResponse import OrderStatusResponse
 
 logger = logging.getLogger(__name__)
-coloredlogs.install(level='INFO', logger=logger, fmt='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+coloredlogs.install(level='DEBUG', logger=logger, fmt='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 @dataclasses.dataclass
 class WikifolioTradingAPI():
@@ -29,6 +30,18 @@ class WikifolioTradingAPI():
         r.raise_for_status()
         logger.debug(f'Session token: {r.json()["sessionToken"]}')
         self.sessionToken = r.json()['sessionToken']
+        
+    def logout(self) -> None:
+        """Logout
+        """
+        headers = {
+            'accept': 'application/json',
+            'sessionToken': self.sessionToken
+        }
+        logger.info('Logging out')
+        r = requests.delete('https://trading-api.wikifolio.com/v1/sessions', headers=headers)
+        r.raise_for_status()
+        logger.info('Logged out')
         
     def list_wikifolios(self) -> List[WikifolioListItem]:
         """List wikifolios
@@ -151,3 +164,176 @@ class WikifolioTradingAPI():
             list_orders += r.json()['results']
         
         return [from_dict(data_class=OrderStatusResponse, data=order) for order in list_orders]
+    
+    def place_limit_order(self, wikifolioSymbol: str, underlying: str, amount: int, limitPrice: float, validUntilDate: datetime.date, side: str, stopPrice: Optional[float] = None) -> str:
+        """Place a limit order
+
+        Args:
+            wikifolioSymbol (str): The wikifolio symbol
+            underlying (str): The ISIN
+            amount (int): The amount
+            limitPrice (float): The limit price
+            validUntilDate (datetime.date): The valid until date, this can't go too far into the future, not sure how far
+            side (str): The side. Possible values: 'buy', 'sell'
+            stopPrice (Optional[float]): The stop price. Defaults to None.
+
+        Returns:
+            str: The order ID
+        """
+        side = side.lower()
+        if side not in ['buy', 'sell']:
+            logger.error(f'Invalid side: {side}, must be "buy" or "sell"')
+            return
+        if side == 'sell' and stopPrice:
+            logger.error('Stop price is only allowed for buy orders')
+            return
+        if stopPrice and side == 'buy':
+            orderType = 'BuyStopLimit'
+        elif side == 'buy':
+            orderType = 'BuyLimit'
+        elif side == 'sell':
+            orderType = 'SellLimit'
+            
+        headers = {
+            'accept': 'application/json',
+            'sessionToken': self.sessionToken
+        }
+        data = {
+            'wikifolioSymbol': wikifolioSymbol,
+            'underlying': underlying,
+            'amount': amount,
+            'limitPrice': limitPrice,
+            'validUntilDate': validUntilDate.strftime('%Y-%m-%d'),
+            'orderType': orderType
+        }
+        if stopPrice:
+            data['stopPrice'] = stopPrice
+        
+        logger.debug(f'Data for limit order: {data}')    
+        logger.info(f'Placing limit order for {wikifolioSymbol}')
+        r = requests.post(f'https://trading-api.wikifolio.com/v1/limitorders', headers=headers, json=data)
+        logger.debug(f'Limit order response: {r.text}')
+        r.raise_for_status()
+        logger.info(f'Placed limit order for {wikifolioSymbol}')
+        
+        return r.json()['orderId']
+    
+    def update_limit_order(self, orderId: str, limitPrice: float, amount: int, validUntilDate: datetime.date, stopPrice: Optional[float] = None) -> None:
+        """Update a limit order
+
+        Args:
+            orderId (str): The order ID
+            limitPrice (float): The limit price
+            amount (int): The amount
+            validUntilDate (datetime.date): The valid until date, this can't go too far into the future, not sure how far
+            stopPrice (Optional[float]): The stop price. Defaults to None.
+        """
+        headers = {
+            'accept': 'application/json',
+            'sessionToken': self.sessionToken
+        }
+        data = {
+            'limitPrice': limitPrice,
+            'amount': amount,
+            'validUntilDate': validUntilDate.strftime('%Y-%m-%d')
+        }
+        if stopPrice:
+            data['stopPrice'] = stopPrice
+        
+        logger.debug(f'Data for updating limit order: {data}')    
+        logger.info(f'Updating limit order {orderId}')
+        r = requests.put(f'https://trading-api.wikifolio.com/v1/limitorders/{orderId}', headers=headers, json=data)
+        logger.debug(f'Update limit order response: {r.text}')
+        r.raise_for_status()
+        logger.info(f'Updated limit order {orderId}')
+        
+    def delete_limit_order(self, orderId: str) -> None:
+        """Delete a limit order
+
+        Args:
+            orderId (str): The order ID
+        """
+        headers = {
+            'accept': 'application/json',
+            'sessionToken': self.sessionToken
+        }
+        logger.info(f'Deleting limit order {orderId}')
+        r = requests.delete(f'https://trading-api.wikifolio.com/v1/limitorders/{orderId}', headers=headers)
+        logger.debug(f'Delete limit order response: {r.text}')
+        r.raise_for_status()
+        logger.info(f'Deleted limit order {orderId}')
+        
+    def get_limit_order(self, orderId: str) -> OrderStatusResponse:
+        """Get a limit order
+
+        Args:
+            orderId (str): The order ID
+
+        Returns:
+            OrderStatusResponse: The order
+        """
+        headers = {
+            'accept': 'application/json',
+            'sessionToken': self.sessionToken
+        }
+        logger.info(f'Getting limit order {orderId}')
+        r = requests.get(f'https://trading-api.wikifolio.com/v1/limitorders/{orderId}', headers=headers)
+        r.raise_for_status()
+        
+        return from_dict(data_class=OrderStatusResponse, data=r.json())
+    
+    def place_quote_order(self, wikifolioSymbol: str, underlying: str, amount: int, side: str) -> str:
+        """Place a quote order
+
+        Args:
+            wikifolioSymbol (str): The wikifolio symbol
+            underlying (str): The ISIN
+            amount (int): The amount
+            side (str): The side. Possible values: 'buy', 'sell'
+
+        Returns:
+            str: The order ID
+        """
+        
+        # Step 1: get quote
+        
+        side = side.lower()
+        if side not in ['buy', 'sell']:
+            logger.error(f'Invalid side: {side}, must be "buy" or "sell"')
+            return
+        if side == 'buy':
+            orderType = 'Buy'
+        elif side == 'sell':
+            orderType = 'Sell'
+            
+        headers = {
+            'accept': 'application/json',
+            'sessionToken': self.sessionToken
+        }
+        data = {
+            'wikifolioSymbol': wikifolioSymbol,
+            'underlying': underlying,
+            'amount': amount,
+            'orderType': orderType
+        }
+        
+        logger.debug(f'Data for quote: {data}')    
+        logger.info(f'Getting quote for {wikifolioSymbol}')
+        r = requests.post(f'https://trading-api.wikifolio.com/v1/quotes', headers=headers, json=data)
+        logger.debug(f'Quote response: {r.text}')
+        r.raise_for_status()
+        logger.info(f'Got quote for {wikifolioSymbol}')
+        
+        # Step 2: place order
+        quoteId = r.json()['quoteId']
+        data = {
+            'quoteId': quoteId
+        }
+        logger.info(f'Placing quote order for {wikifolioSymbol}')
+        r = requests.post(f'https://trading-api.wikifolio.com/v1/quoteorders', headers=headers, json=data)
+        logger.debug(f'Quote order response: {r.text}')
+        r.raise_for_status()
+        logger.info(f'Placed quote order for {wikifolioSymbol}')
+        
+        return r.json()['orderId']
+        
